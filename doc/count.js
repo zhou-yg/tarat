3c1efe4a6e5f7675fe45f2f20cc5d43aedc0f771
zhouyunge <zhouyunge@kuaishou.com>
2022-05-20 17:02:07 +0800

    feat: update

--- doc/count.js
+++ doc/count.js
@@ -3,3 +3,4 @@ fs =  require('fs')
 const n = fs.readdirSync('./design').filter(f => /\.md/.test(f)).map(f => {
   return fs.readFileSync(`./design/${f}`).toString().split('').filter(char => !!char).length
 }).reduce((p, n) => p + n)
+console.log('n: ', n);
