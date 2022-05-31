3e56bab1cdc73f204dba2cd021739440f159f940
zhouyunge <zhouyunge@kuaishou.com>
2022-05-31 16:36:35 +0800

    feat: add react example and rename old

--- packages/example/individual-server-client(axii)/client/index.tsx
+++ packages/example/individual-server-client(axii)/client/index.tsx
@@ -14,24 +14,25 @@ function Plus () {
   const v1  = r.s1
   const v2  = r.s2
 
-
   function plus () {
     r.add(1)
+  }
+
+  function plusV2 {
+    v2.value++
   }
 
   return (
     <plus block block-padding={8}>
       <div block block-padding={8}>      
-        v1:{() => v1.num}
+        v1.num:{() => v1.num}
       </div>
       <div block block-padding={8}>      
         v2:{() => v2.value}
       </div>
       <div block block-padding={8}>
         <button onClick={plus}>all plus 1</button>
-        <button block onClick={() => {
-          v2.value++
-        }}>add v2</button>
+        <button block onClick={plusV2}>plus v2</button>
       </div>
     </plus>
   )
