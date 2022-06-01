edc20c42d0626f1451eca8d93ff8059c1c396d9f
zhouyunge <zhouyunge@kuaishou.com>
2022-06-01 22:52:29 +0800

    feat:  initialize taorot-server

--- packages/server/src/basicServer.ts
+++ packages/server/src/basicServer.ts
@@ -1,13 +1,11 @@
 import Application from "koa";
 import koaBody from 'koa-body'
 import cors from '@koa/cors'
-import tarotRunner from "./middlewares/tarotRunner";
 
-export default function (app: Application) {
+export function setupBasicServer (app: Application) {
 
   app.use(koaBody())
   app.use(cors())
-  app.use(tarotRunner())
 
   return app
 }
