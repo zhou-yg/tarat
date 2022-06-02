3a14afa17491574347e8e185f1cdeecb3bad52f5
zhouyunge <zhouyunge@kuaishou.com>
2022-06-02 20:02:28 +0800

    fix: lint error

--- packages/example/individual-server-client(axii)/client/index.tsx
+++ packages/example/individual-server-client(axii)/client/index.tsx
@@ -15,33 +15,32 @@ setHookAdaptor(axii, 'axii');
 function Plus () {
 
   const r = useHook(hook)
-  
-  const v1  = r.s1
-  const v2  = r.s2
-  console.log('v2: ', v1(), v2());
+
+  const { s1, s2 } = r
 
   function plus () {
     r.add(1)
   }
 
   function plusV2 () {
-    v2((v) => {
+    s2((v) => {
       return v + 1
     })
   }
 
   return (
     <plus block block-padding={8}>
-      <div block block-padding={8}>      
-        v1.num:{() => v1().num}
-      </div>
-      <div block block-padding={8}>      
-        v2:{() => v2().value}
-      </div>
-      <div block block-padding={8}>
+      <row block block-padding={8}>      
+        v1.num:{() => s1().num}
+      </row>
+      <row block block-padding={8}>      
+        v2:{() => s2().value}
+      </row>
+      <row block block-padding={8}>
         <button onClick={plus}>all plus 1</button>
-        <button block onClick={plusV2}>plus v2</button>
-      </div>
+        <br/>
+        <button onClick={plusV2}>plus v2</button>
+      </row>
     </plus>
   )
 }
