a099225f0c7646bb7350789532d3089a8841ed88
zhouyunge <zhouyunge@kuaishou.com>
2022-06-02 20:01:18 +0800

    feat: use new axii hook connect

--- packages/example/individual-server-client(axii)/client/index.tsx
+++ packages/example/individual-server-client(axii)/client/index.tsx
@@ -1,12 +1,16 @@
+/** @jsx createElement */
 import {
   createElement,
   atom,
   render
 } from 'axii'
 
-
 import hook from '../hooks/hook'
-import useHook from '../connect/useAxiiHook'
+import { setHookAdaptor,  useHook } from '@tarot-run/core'
+import * as axii from 'axii'
+import '../connect/useAxiiHookRuntime'
+
+setHookAdaptor(axii, 'axii');
 
 function Plus () {
 
@@ -14,22 +18,25 @@ function Plus () {
   
   const v1  = r.s1
   const v2  = r.s2
+  console.log('v2: ', v1(), v2());
 
   function plus () {
     r.add(1)
   }
 
-  function plusV2 {
-    v2.value++
+  function plusV2 () {
+    v2((v) => {
+      return v + 1
+    })
   }
 
   return (
     <plus block block-padding={8}>
       <div block block-padding={8}>      
-        v1.num:{() => v1.num}
+        v1.num:{() => v1().num}
       </div>
       <div block block-padding={8}>      
-        v2:{() => v2.value}
+        v2:{() => v2().value}
       </div>
       <div block block-padding={8}>
         <button onClick={plus}>all plus 1</button>
