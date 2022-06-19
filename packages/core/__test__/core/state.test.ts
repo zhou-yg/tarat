704048cb770966ad0980621efa5c081eef35f4eb
zhouyunge <zhouyunge@kuaishou.com>
2022-06-19 17:02:35 +0800

    feat(core): add u-t with context

--- packages/core/__test__/core/state.test.ts
+++ packages/core/__test__/core/state.test.ts
@@ -1,32 +1,56 @@
 import {
+  IHookContext,
   Runner,
-} from '../../src/core'
+} from '../../src/'
 
 import * as mockBM from '../mockBM'
 
 describe('state', () => {
-  it('use plain object', () => {
-    const runner = new Runner(mockBM.plainObjectState)
-    const args: [{num1: number}, number] = [
-      { num1: 0 },
-      10
-    ]
-    const result = runner.init(args)
-
-    expect(result.s1()).toEqual(args[0])
-    expect(result.s2()).toEqual(args[1])
+  describe('mount state', () => {
+    it('use plain object', () => {
+      const runner = new Runner(mockBM.plainObjectState)
+      const args: [{num1: number}, number] = [
+        { num1: 0 },
+        10
+      ]
+      const result = runner.init(args)
+  
+      expect(result.s1()).toEqual(args[0])
+      expect(result.s2()).toEqual(args[1])
+    })
+    it('watch state changing', () => {
+      const runner = new Runner(mockBM.plainObjectState)
+      const args: [{num1: number}, number] = [
+        { num1: 0 },
+        10
+      ]
+      const result = runner.init(args)
+  
+      
+  
+      expect(result.s1()).toEqual(args[0])
+      expect(result.s2()).toEqual(args[1])
+    })
   })
-  it('watch state changing', () => {
-    const runner = new Runner(mockBM.plainObjectState)
-    const args: [{num1: number}, number] = [
-      { num1: 0 },
-      10
-    ]
-    const result = runner.init(args)
 
-    
-
-    expect(result.s1()).toEqual(args[0])
-    expect(result.s2()).toEqual(args[1])
+  describe('update state', () => {
+    it('init with context', () => {
+      const runner = new Runner(mockBM.plainObjectState)
+      const args: [{num1: number}, number] = [
+        { num1: 0 },
+        10
+      ]
+      const cd: IHookContext['data'] = [
+        ['data', { num1: 1 }],
+        ['data', 12]
+      ]
+      const context = mockBM.initContext({
+        data: cd
+      })
+      const result = runner.init(args, context)
+  
+      expect(result.s1()).toEqual(cd[0][1])
+      expect(result.s2()).toEqual(cd[1][1])
+    })
   })
 })
