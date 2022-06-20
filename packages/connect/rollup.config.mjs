81d87b5bd56136e306a14548d46930ed1741b83d
zhouyunge <zhouyunge@kuaishou.com>
2022-06-20 19:37:40 +0800

    feat(connect): making  connect independent

---
+++ packages/connect/rollup.config.mjs
@@ -0,0 +1,29 @@
+import tsPlugin from 'rollup-plugin-typescript2'
+import dts from "rollup-plugin-dts"
+import replace from '@rollup/plugin-replace';
+// import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default [
+  {
+    plugins: [
+      tsPlugin({
+        clean: true,
+        tsconfig: './tsconfig.json',
+      }),
+    ],
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'esm'
+    },
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      { file: "dist/index.d.ts", format: "es" }
+    ],
+    plugins: [
+      dts(),
+    ],
+  }
+]
