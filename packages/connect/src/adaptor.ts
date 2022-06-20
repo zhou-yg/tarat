161301167146ee92189779030de5ff017d6c0040
zhouyunge <zhouyunge@kuaishou.com>
2022-06-20 19:40:57 +0800

    feat(connect): intergrate all connect like

---
+++ packages/connect/src/adaptor.ts
@@ -0,0 +1,21 @@
+import { useAxiiHook } from './axii'
+import { useReactHook } from './react'
+
+let hookAdaptorRuntime: any = null
+let hookAdaptorType: 'react' | 'axii' | null = null
+export function setHookAdaptor(runtime: any, type: 'react' | 'axii') {
+  hookAdaptorRuntime = runtime
+  hookAdaptorType = type
+}
+
+export type BM = (...prop: any) => any
+
+export function useHook<T extends BM>(bm: T): ReturnType<T> | undefined {
+  switch (hookAdaptorType) {
+    case 'react':
+      return useReactHook(hookAdaptorRuntime, bm)
+    case 'axii':
+      return useAxiiHook(hookAdaptorRuntime, bm)
+  }
+}
+export const useTarat = useHook
