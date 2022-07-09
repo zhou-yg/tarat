ab231c7e7ce16e99050626b238b15b416e961a4c
zhou-yg <zhouyunge@kuaishou.com>
2022-07-09 16:03:04 +0800

    feat: clean

--- packages/example/auto-dep-map/index.ts
+++
@@ -1,9 +0,0 @@
-import { parse } from "./parser.js";
-import { readFileSync, writeFileSync } from 'fs'
-
-
-const hookJs = readFileSync('./hooks/login.js').toString()
-
-const deps = parse(hookJs)
-
-writeFileSync('./login.deps.json', JSON.stringify(deps, null, 2))
