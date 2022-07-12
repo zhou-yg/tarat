cbba583abd15dc4c1d6f38353db5ea4630dfebab
zhou-yg <zhou.y.g890@gmail.com>
2022-07-12 22:00:46 +0800

    feat(server): support nest route

--- packages/server/src/util.ts
+++ packages/server/src/util.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import rimraf from 'rimraf'
+import { IViewConfig } from './config'
 
 export function loadJSON (f: string) {
   return JSON.parse(fs.readFileSync(f).toString())
@@ -10,4 +11,21 @@ export function emptyDirectory (dir: string) {
   rimraf.sync(dir)
 
   fs.mkdirSync(dir)
+}
+
+export function getDefeaultRoute (pages: IViewConfig[]) {
+  let root: IViewConfig = pages[0]
+  pages.forEach(p => {
+    const p1 = p.path.split('/')
+    const p2 = root.path.split('/')
+    if (p1.length <= p2.length) {
+      if (p1.length === p2.length) {
+        root = p.index ? p : root
+      } else {
+        root = p
+      }
+    }
+  })
+
+  return root.name === 'index' ? '' : root.name
 }
