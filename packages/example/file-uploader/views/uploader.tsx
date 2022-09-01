f23f012732aadad91e9eebc46d81a079310d4c56
zhou-yg <zhou.y.g890@gmail.com>
2022-09-01 10:16:08 +0800

    feat(example): update uploader

--- packages/example/file-uploader/views/uploader.tsx
+++ packages/example/file-uploader/views/uploader.tsx
@@ -1,13 +1,13 @@
 import { useTarat } from 'tarat-connect'
 import React, { useState } from 'react'
 
-const Todos = () => {
+const Uploader = () => {
 
   return (
     <div>
       <input type="file" defaultValue="" onChange={e => {
-        console.log('e: ', e.target.value, typeof e.target.value);
-        console.log('e: ', e.target.files, typeof e.target.files[0], e.target.files[0] instanceof File);
+        // console.log('e: ', e.target.value, typeof e.target.value);
+        // console.log('e: ', e.target.files, typeof e.target.files[0], e.target.files[0] instanceof File);
         const fd = new FormData()
         fd.append('myFile', e.target.files[0])
         fd.append('fileName', e.target.files[0].name)
@@ -20,4 +20,4 @@ const Todos = () => {
   )
 }
 
-export default Todos
+export default Uploader
