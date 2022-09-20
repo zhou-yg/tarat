ed30de764be50ce38376769a802f3e7b65cddc04
zhouyunge <zhouyunge@kuaishou.com>
2022-09-20 09:18:33 +0800

    feat(create): add init

--- packages/create/src/index.js
+++ packages/create/src/index.js
@@ -1,19 +1,98 @@
 const inquirer = require('inquirer')
+const { writeFile, existsSync, mkdir, mkdirSync, rm } = require('node:fs')
+const https = require('node:https')
+const { join } = require('node:path')
+const StreamZip = require('node-stream-zip');
+const {} = require('child_process');
+const { spawn } = require('node:child_process');
+
+const taratTemplatesZipURL = 'https://codeload.github.com/zhou-yg/tarat-templates/zip/refs/heads/master'
+
+const cacheZip = join(__dirname, './tarat-templates.zip')
+
+const regularManagers = ['pnpm', 'yarn', 'npm']
+
+async function downloadZip () {
+  return new Promise((resolve, reject) => {
+    https.get(taratTemplatesZipURL, res => {
+      const { statusCode } = res
+      if (statusCode !== 200) {
+        console.error(`Request failed: ${statusCode}`);
+        res.resume();
+        return;
+      }
+      let buffers = []
+      let size = 0
+      res.on('data', (chunk) => {
+        buffers.push(chunk)
+        size += chunk.length
+      })
+      res.on('end', () => {
+        const result = Buffer.concat(buffers, size)
+        writeFile(cacheZip, result, (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        })
+      })
+    })
+  })
+}
+
+async function create(cwd, options) {
+  await downloadZip()
+
+  const projectDir = join(__dirname, options.name)
+
+  const zip = new StreamZip.async({ file: cacheZip })
+  if (!existsSync(projectDir)) {
+    mkdirSync(projectDir)
+  }
+
+  const subDirPath = options.useTs ? 'ts' : 'js'
+
+  await zip.extract(`tarat-templates-master/${subDirPath}`, projectDir)
+  await zip.close()
+
+  const { installer } = await inquirer
+    .prompt([
+      {
+        name: 'installer',
+        message: 'Do you want me to run `install`',
+        type: 'list',
+        choices: ['dont do this', ...regularManagers]
+      },
+    ])
+  if (regularManagers.includes(installer)) {
+    spawn(`${installer}`, ['install'], {
+      stdio: ['pipe', process.stdout, process.stderr],
+      cwd: projectDir
+    })
+  }
+}
+
 
 inquirer
   .prompt([
     {
-      nmae: 'name',
+      name: 'name',
       message: 'Please input your project name',
       type: 'input',
-      default: 'my-tarat-unit'
+      default: 'my-tarat-module'
     },
     {
-      name: 'language',
+      name: 'lang',
       message: 'Which language you prefer',
       type: 'list',
       choices: ['typescript', 'javascript']
     },
-  ]).then(answers => {
-    console.log('answers: ', answers);
+  ]).then(({ lang, name }) => {
+    create(process.cwd(), { useTs: lang === 'typescript', name })
+      .finally(() => {
+        rm(cacheZip, (err) => {
+          if (err) console.error(err)
+        })
+      })
   })
