eb26c32a89590c803a0b2317aba35bceb4831891
zhouyunge <zhouyunge@kuaishou.com>
2022-10-07 16:54:56 +0800

    release: tarat v0.0.48

--- packages/example/file-uploader/drivers/uploader.ts
+++ packages/example/file-uploader/drivers/uploader.ts
@@ -6,8 +6,8 @@ import {
   writePrisma,
   inputComputeInServer,
 } from 'tarat/core'
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 import indexes from '@/models/indexes.json'
 
 export default function uploader<T> () {
